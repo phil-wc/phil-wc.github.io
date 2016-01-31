a340b7fd3eb7bb6f41546b8e8a8ed93e549a7db9
Thitisarn <jmsarn@gmail.com>
2016-01-31 10:28:55 +0700

    added javascript to replace text hyperlink

---
+++ assets/js/replace-on-page-not-found.js
@@ -0,0 +1,7 @@
+$(function() {
+  var htmlMessage = '<p class="subheading">Due to brand new website, you might click an old link from older website. ' +
+                    'For reading old articles please go to ' +
+                    '<a class="page-not-found-hyperlink" href="http://phil-wcofficial.tumblr.com/">http://phil-wcofficial.tumblr.com/</a></p>';
+  $('.site-heading .subheading').replaceWith(htmlMessage);
+});
+
