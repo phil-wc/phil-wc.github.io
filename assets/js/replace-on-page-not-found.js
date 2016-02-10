1074542e1f289b9effdc1a2aa6886ead5d93c286
Chapavich <phil_wc@Chapavichs-MacBook-Pro.local>
2016-02-10 11:39:31 -0800

    404fix

--- assets/js/replace-on-page-not-found.js
+++ assets/js/replace-on-page-not-found.js
@@ -1,7 +1,7 @@
 $(function() {
-  var htmlMessage = '<p class="subheading">Due to brand new website, you might click an old link from older website. ' +
-                    'For reading old articles please go to ' +
-                    '<a class="page-not-found-hyperlink" href="http://phil-wcofficial.tumblr.com/">http://phil-wcofficial.tumblr.com/</a></p>';
+  var htmlMessage = '<p class="subheading">Due to my brand new website, you might click an old link from older permalink url. ' +
+                    'For reading old articles please go to my blog archive ' +
+                    '<p><a class="button-solid" href="http://phil-wcofficial.tumblr.com/">phil-wcofficial.tumblr.com</a></p>' +
+                    'Popular Article: <a class="page-not-found-hyperlink" href="http://www.phil-wc.com/2016/01/27/best-way-to-coop-composer/">Best way to cooperate your film project with a composer.</a>';
   $('.site-heading .subheading').replaceWith(htmlMessage);
 });
-
